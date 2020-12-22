7ca0013d9244455e244503ea170ebf19d5663dfb
Markrose555 <mark@ruzinov.com>
2020-12-22 18:35:55 +0100

    Improved display of slider values

--- final/Problem5/divDimensions.php
+++ final/Problem5/divDimensions.php
@@ -22,13 +22,13 @@
 <body>
   <form action="divDimensions.php" method="GET">
 		<label>Width:</label> <input type="range" min="0" max="500" name="w" id="w" value="<?php echo $w;?>" oninput="wOutput.value = w.value">
-    <output name="wOutput"></output>px <br />
+    <output name="wOutput"><?php echo $w; ?></output>px <br />
 		<label>Height:</label> <input type="range" min="0" max="500" name="h" id="h" value="<?php echo $h;?>" oninput="hOutput.value = h.value" >
-    <output name="hOutput"></output>px <br />
+    <output name="hOutput"><?php echo $h; ?></output>px <br />
     <label>Left Offset:</label> <input type="range" min="0" max="500" name="l" id="l" value="<?php echo $l;?>" oninput="lOutput.value = l.value" >
-    <output name="lOutput"></output>px <br />
+    <output name="lOutput"><?php echo $l; ?></output>px <br />
     <label>Top Offset:</label> <input type="range" min="0" max="500" name="t" id="t" value="<?php echo $t;?>" oninput="tOutput.value = t.value" >
-    <output name="tOutput"></output>px <br /><br />
+    <output name="tOutput"><?php echo $t; ?></output>px <br /><br />
 		<input type="submit" value="Refresh Box Shape">
   </form>
   <div style="background-color:black; width:<?php echo $w; ?>px; height:<?php echo $h; ?>px; margin-left:<?php echo $l; ?>px; margin-top:<?php echo $t; ?>px;" />
